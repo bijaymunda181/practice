075b1469f57fc515533c8ca4639dd0b250370d15
Bijay <bijay78082@gmail.com>
2026-09-01 16:52:07 +0530

    terraform

--- function/file-function/main.tf
+++ function/file-function/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "main" {
   ami           = "ami-081b0a6eac00b4f53"
-  instance_type = "t2.small"
+  instance_type = "t3.small"
 
   user_data = file("${path.module}/install.sh")
   tags = {
