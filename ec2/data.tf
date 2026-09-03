c729054c30d14f61d7099984d544ec749674505c
Bijay <bijay78082@gmail.com>
2026-09-03 15:16:13 +0530

    terraform

--- ec2/data.tf
+++ ec2/data.tf
@@ -1,7 +1,12 @@
 variable "security_group_id" {}
+variable "vpc_id" {}
 
 data "aws_security_group" "launch-wizard-1" {
   id = var.security_group_id
+}
+
+data "aws_vpc" "selected" {
+  id = var.vpc_id
 }
 
 
