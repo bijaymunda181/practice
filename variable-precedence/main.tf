c6b9e036d5fe659ede262b9cf15e75b0a966407f
Bijay <bijay78082@gmail.com>
2026-09-07 11:33:15 +0530

    terraform

---
+++ variable-precedence/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
+    }
+  }
+}
+
+resource "aws_instance" "precedence-check" {
+  ami = "ami-025b6f0b1ac2ef9f7"
+  instance_type = var.instance_type
+}
