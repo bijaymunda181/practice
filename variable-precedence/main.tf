a80570938a05bf148579cba456ea72c12095f93d
Bijay <bijay78082@gmail.com>
2026-09-07 11:38:10 +0530

    terraform

--- variable-precedence/main.tf
+++ variable-precedence/main.tf
@@ -10,4 +10,8 @@ terraform {
 resource "aws_instance" "precedence-check" {
   ami = "ami-025b6f0b1ac2ef9f7"
   instance_type = var.instance_type
+
+  tags = {
+    Name = "variable-precedence"
+  }
 }
