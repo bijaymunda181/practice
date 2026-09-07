05aa0b95cf63f0e948998598720157c17491554e
Bijay <bijay78082@gmail.com>
2026-09-07 15:08:09 +0530

    terraform

--- data-types/map-data-type/main.tf
+++ data-types/map-data-type/main.tf
@@ -1,5 +1,7 @@
 resource "aws_instance" "map-data-type" {
-  ami = var.ami["prod"]
-  instance_type = var.instance_type["prod"]
+  ami = var.ami["dev"]
+  instance_type = var.instance_type["dev"]
+
+  }
 }
 
--- data-types/map-data-type/vars.tf
+++ data-types/map-data-type/vars.tf
@@ -10,3 +10,22 @@ variable "instance_type" {
     dev  = "t3.micro"
   }
 }
+
+
+
+# variable "components" {
+#   default = {
+#     prod = {
+#       ami = "ami-081b0a6eac00b4f53"
+#       instance_type = "t3.small"
+#     }
+#     dev = {
+#       ami = "ami-025b6f0b1ac2ef9f7"
+#       instance_type = "t3.micro"
+#     }
+#     tags ={
+#       name =
+#     }
+#   }
+# }
+
