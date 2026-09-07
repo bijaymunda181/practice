d0de581ac6de3c548eda653677976da35c6b5f6c
Bijay <bijay78082@gmail.com>
2026-09-07 15:23:55 +0530

    terraform

--- data-types/map-data-type/main.tf
+++ data-types/map-data-type/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "map-data-type" {
   ami = var.ami["dev"]
   instance_type = var.instance_type["dev"]
+  availability_zone = var.availability_zone[2]
  }
 
 
--- data-types/map-data-type/vars.tf
+++ data-types/map-data-type/vars.tf
@@ -11,6 +11,10 @@ variable "instance_type" {
   }
 }
 
+variable "availability_zone" {
+  default =  ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
 
 
 # variable "components" {
