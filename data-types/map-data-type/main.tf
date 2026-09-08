abbf6fd69d64c575e3d1413f192031adcb5c60ea
Bijay <bijay78082@gmail.com>
2026-09-08 11:16:24 +0530

    terraform

--- data-types/map-data-type/main.tf
+++ data-types/map-data-type/main.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "map-data-type" {
+  count = 2
   ami = var.ami["dev"]
   instance_type = var.instance_type["dev"]
-  availability_zone = var.availability_zone[count.index]
+  availability_zone = var.availability_zone[0]
 
  }
 
