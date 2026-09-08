cb475aa694bec8f0df422ba5a16cad5ef7811e81
Bijay <bijay78082@gmail.com>
2026-09-08 11:13:40 +0530

    terraform

--- data-types/map-data-type/main.tf
+++ data-types/map-data-type/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "map-data-type" {
   ami = var.ami["dev"]
   instance_type = var.instance_type["dev"]
-  availability_zone = var.availability_zone[0]
+  availability_zone = var.availability_zone[count.index]
 
  }
 
--- data-types/map-data-type/vars.tf
+++ data-types/map-data-type/vars.tf
@@ -12,7 +12,7 @@ variable "instance_type" {
 }
 
 variable "availability_zone" {
-  default =  ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default =  ["us-east-1a", "us-east-1b", "us-east-1c"]   // It is a list data type
 }
 
 
