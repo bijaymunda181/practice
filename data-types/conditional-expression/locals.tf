ee9c31b005cbfbc3716ee29f8f96c8314dd9ff8d
Bijay <bijay78082@gmail.com>
2026-09-08 12:32:27 +0530

    terraform

--- data-types/conditional-expression/locals.tf
+++ data-types/conditional-expression/locals.tf
@@ -1,6 +1,5 @@
 locals {
-  common_tags = {
-    owner = "ABC"
-    Name  = "EC2"
-  }
+  environment = "dev"
+  project     = "myapp"
+  instance_name = "${local.project}-${local.environment}"
 }
--- data-types/conditional-expression/main.tf
+++ data-types/conditional-expression/main.tf
@@ -3,6 +3,9 @@ resource "aws_instance" "dev" {
   count = var.env == "prod" ? 1 : 0
   ami = "ami-081b0a6eac00b4f53"
   instance_type = "t3.micro"
-  tags = local.common_tags
+  tags = {
+    Name = local.instance_name
+    environment = local.environment
+  }
 }
 
