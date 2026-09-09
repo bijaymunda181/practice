cb0f4e29b3a11615d8d85f326be6c86b42d548e4
Bijay <bijay78082@gmail.com>
2026-09-09 12:18:00 +0530

    terraform

--- function/file-function/file-function-v2/main.tf
+++ function/file-function/file-function-v2/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
 
 resource "aws_key_pair" "login_key" {
   key_name = "login_key"
