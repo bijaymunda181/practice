500717e55affdb53f6df02aa71903ec2e479f062
Bijay <bijay78082@gmail.com>
2026-09-09 10:48:26 +0530

    terraform

--- function/element-function/main.tf
+++ function/element-function/main.tf
@@ -2,5 +2,8 @@
 resource "aws_instance" "element-f-check" {
   ami = "ami-025b6f0b1ac2ef9f7"
   instance_type = element(var.instance_type, 2)
-  tags          = element(var.tags, 2 )
+
+  tags = {
+    Name = element(var.tags, 2 )
+  }
 }
