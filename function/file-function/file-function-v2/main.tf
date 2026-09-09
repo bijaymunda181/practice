603dd3d7258096102cc6c12bf1263bbda398632d
Bijay <bijay78082@gmail.com>
2026-09-09 12:06:46 +0530

    terraform

---
+++ function/file-function/file-function-v2/main.tf
@@ -0,0 +1,15 @@
+
+resource "aws_key_pair" "login_key" {
+  key_name = "login_key"
+  public_key = file ("${path.module}/id_rsa_pub")
+}
+
+resource "aws_instance" "app" {
+  ami = lookup(var.ami, var.region )
+  instance_type = "t3.small"
+  key_name = aws_key_pair.login_key.key_name
+  count = 2
+  tags = {
+    Name = element(var.tags, count.index )
+  }
+}
---
+++ function/file-function/file-function-v2/vars.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  default = {
+    us-east-1  = "ami-081b0a6eac00b4f53",
+    ap-south-1 = "ami-025b6f0b1ac2ef9f7"
+  }
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "tags" {
+  default = ["serve1", "server2"]
+}
+
