d2220f6c13ff8f1e190efebef5554e77d4f75712
Bijay <bijay78082@gmail.com>
2026-09-18 11:16:28 +0530

    terraform

--- ec2/main.tf
+++ ec2/main.tf
@@ -10,9 +10,11 @@ terraform {
 
 
 resource "aws_instance" "New" {
+  count = 2
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.launch-wizard-1.id]
+  user_data = file("${path.module}/install.sh")
 
   tags = {
     Name = "tfvars-server"
