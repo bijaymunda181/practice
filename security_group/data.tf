5c267bc716518ed4324ddf438b49c20d6d047028
Bijay <bijay78082@gmail.com>
2026-09-19 13:10:55 +0530

    terraform

---
+++ security_group/data.tf
@@ -0,0 +1,5 @@
+data "aws_vpc" "default" {
+  id = aws_vpc_security_group_ingress_rule
+}
+
+
--- security_group/main.tf
+++ security_group/main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "vpc-0b8599cb522da7269"
+  cidr_ipv4         = "172.31.0.0/16"
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
