46c7d11c6fa1776e8fa6347d08f10ed0b81b14ed
Bijay <bijay78082@gmail.com>
2026-09-19 13:02:57 +0530

    terraform

--- security_group/main.tf
+++ security_group/main.tf
@@ -6,10 +6,12 @@ resource "aws_security_group" "allow_tls" {
   tags = {
     Name = "New-SG"
   }
-  egress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "-1"
+}
 
-  }
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "vpc-0b8599cb522da7269"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
 }
