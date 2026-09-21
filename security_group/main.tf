65dba052b5a5fd850af3c1f40b4afdd505d8ad5b
Bijay <bijay78082@gmail.com>
2026-09-21 12:59:33 +0530

    terrfaorm

--- security_group/main.tf
+++ security_group/main.tf
@@ -8,22 +8,23 @@ resource "aws_security_group" "dynamic_sg" {
     for_each = var.ingress_ports
 
     content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
+      from_port = ingress.value
+      to_port   = ingress.value
+      protocol  = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
     dynamic "egress" {
       for_each = var.egress_ports
 
       content {
-        from_port   = egress.value
-        to_port     = egress.value
-        protocol    = "tcp"
+        from_port = egress.value
+        to_port   = egress.value
+        protocol  = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
       }
     }
   }
+}
 
 
 
