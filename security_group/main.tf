3f97ba7053f1de8a8a06675e25ce9aa089f1a5ff
Bijay <bijay78082@gmail.com>
2026-09-21 12:20:38 +0530

    terrfaorm

--- security_group/main.tf
+++ security_group/main.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "allow_tls" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = "sg-0902a340f4e53a767"
+  security_group_id = aws_security_group.allow_tls.id
   cidr_ipv4         = "172.31.0.0/16"
   from_port         = 443
   ip_protocol       = "tcp"
