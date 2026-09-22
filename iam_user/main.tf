cde7db1f8a6c89edb4ed82aec5e19f69a47b9876
Bijay <bijay78082@gmail.com>
2026-09-22 12:21:05 +0530

    terraform

--- iam_user/main.tf
+++ iam_user/main.tf
@@ -1,11 +1,9 @@
 resource "aws_iam_user" "lb" {
   count = 3
-  name = "iam_user.${count.index}"
+  name = "iam_user-${count.index+1}"
   path = "/system/"
 
-  tags = {
-    Name = "user-${count.index+1}"
-  }
+
 }
 
 
