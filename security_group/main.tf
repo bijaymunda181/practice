2c7cb6a5a3cf0b02d636e7fee8a4b712ef97af5a
Bijay <bijay78082@gmail.com>
2026-09-22 11:20:23 +0530

    terraform

--- security_group/main.tf
+++ security_group/main.tf
@@ -6,20 +6,22 @@ resource "aws_security_group" "dynamic_sg" {
 
   dynamic "ingress" {
     for_each = var.ingress_ports
+    iterator = port
 
     content {
-      from_port = ingress.value
-      to_port   = ingress.value
+      from_port = port.value
+      to_port   = port.value
       protocol  = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
     dynamic "egress" {
       for_each = var.egress_ports
+      iterator = port
 
       content {
-        from_port = egress.value
-        to_port   = egress.value
+        from_port = port.value
+        to_port   = port.value
         protocol  = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
       }
