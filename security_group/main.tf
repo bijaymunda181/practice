33519e7607765767b051c0c3153e208075e401e2
Bijay <bijay78082@gmail.com>
2026-09-22 11:48:02 +0530

    terraform

--- security_group/main.tf
+++ security_group/main.tf
@@ -3,6 +3,9 @@ resource "aws_instance" "my_instance" {
   ami = "ami-00adafae70b8029d8"
   instance_type = "t3.small"
   vpc_security_group_ids = [aws_security_group.dynamic_sg.id]
+  tags = {
+    Name = "My_instance"
+  }
 }
 
 resource "aws_security_group" "dynamic_sg" {
