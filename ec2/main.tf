e656aadacf6949322df4dd5d7164b1ea2073cf28
Bijay <bijay78082@gmail.com>
2026-09-24 13:35:08 +0530

    terraform

--- ec2/main.tf
+++ ec2/main.tf
@@ -10,11 +10,14 @@
 
 
 resource "aws_instance" "New" {
-  count = 2
+  count = 3
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.launch-wizard-1.id]
   user_data = file("${path.module}/install.sh")
+  lifecycle {
+    ignore_changes = [tags]
+  }
   tags = {
     PatchGroup = "sit-1"
   }
