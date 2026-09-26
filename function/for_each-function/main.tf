caa42a7a86aa85a8fbadcc33e06e5c71ff9d43c0
Bijay <bijay78082@gmail.com>
2026-09-26 16:05:51 +0530

    terraform

--- function/for_each-function/main.tf
+++ function/for_each-function/main.tf
@@ -1,4 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
 resource "aws_iam_user" "prod-users" {
   for_each = toset(["user-1", "user-2", "user-3", "user-1"])
   name = each.key
+}
+
+resource "aws_instance" "ec2" {
+  for_each = {
+    key1 = "t3.micro"
+    key2 = "t3.small"
+  }
+  instance_type = each.value
+  key_name = each.key
+  tags = {
+    Name  = each.value
+  }
 }
