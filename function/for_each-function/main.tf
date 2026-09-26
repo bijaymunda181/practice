9d05bf414e49b6a074ca717dd7089d0bd51a0dd5
Bijay <bijay78082@gmail.com>
2026-09-26 16:07:08 +0530

    terraform

--- function/for_each-function/main.tf
+++ function/for_each-function/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.54.1"
-    }
-  }
-}
 resource "aws_iam_user" "prod-users" {
   for_each = toset(["user-1", "user-2", "user-3", "user-1"])
   name = each.key
